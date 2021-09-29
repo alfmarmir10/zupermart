4d661853013f437b916947f5f7182322787afcad
alfmarmir10 <alfmar100391@icloud.com>
2021-09-29 17:55:46 -0500

    Remove FirebaseUI issue

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -4,16 +4,12 @@ import Logo from '../assets/img/Logo.png'
 import '../styles/global_styles.css';
 import '../styles/SignIn/signIn_styles.css';
 import SideBar from '../components/GLOBAL/SideBar';
-import { startFirebaseUI } from '../firebase/config';
 
 
 const SignIn = () => {
   const {User, setUser} = useContext(UserContext);
   console.log(User);
 
-  useEffect(() => {
-    startFirebaseUI('#firebaseui');
-  }, [])
 
   return (
     <div className="views-main-container">
